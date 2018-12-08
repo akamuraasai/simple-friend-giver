0808ab05bb822d45dfdd5efb36cf245c4407fc09
Jonathan Willian <jonathan.willian.tod@gmail.com>
2018-12-08 04:59:14 -0200

    Updates

---
+++ precache-manifest.184a560f35a12b875ebff771410a3deb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a1e8d4081e2302e3da",
+    "url": "/simple-friend-giver/static/js/runtime~main.97a1e8d4.js"
+  },
+  {
+    "revision": "b23f6181120eb7213682",
+    "url": "/simple-friend-giver/static/js/main.b23f6181.chunk.js"
+  },
+  {
+    "revision": "be5d7e6358e4f750fd77",
+    "url": "/simple-friend-giver/static/js/1.be5d7e63.chunk.js"
+  },
+  {
+    "revision": "ed75dfe404842324177c602a726bb68c",
+    "url": "/simple-friend-giver/index.html"
+  }
+];
